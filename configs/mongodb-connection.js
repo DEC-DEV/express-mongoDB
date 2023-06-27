43ab575242b72a1706ecb8dadcfba6559c1e90ff
DEC-DEV <pdh.december@gmail.com>
2023-06-27 17:18:07 +0900

    modify

--- configs/mongodb-connection.js
+++ configs/mongodb-connection.js
@@ -1,5 +1,5 @@
 const { MongoClient} = require("mongodb"); // 1. 몽고디비 연결 주소
-const uri = "mongodb+srv://donghanpark:9donghan*12@cluster0.pjj2y3s.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://donghanpark:<password>@cluster0.pjj2y3s.mongodb.net/?retryWrites=true&w=majority";
 
 module.exports = function (callback) { // 2. 몽고디비 커넥션 연결 함수 반환
   return MongoClient.connect(uri, callback);
